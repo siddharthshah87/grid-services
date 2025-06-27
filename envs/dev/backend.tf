bf3089b9966faed231b8aa5e3082b133a34ca366
Siddharth Shah <siddharthshah87@gmail.com>
2025-06-26 21:24:21 -0700

    fixed typo

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-grid-services-923675928909"
     key            = "dev/terraform.tfstate"
-    region         = "us-west-1"
+    region         = "us-west-2"
     use_lockfile   = true
     encrypt        = true
   }
--- envs/dev/terraform_init.sh
+++ envs/dev/terraform_init.sh
@@ -1,4 +1,9 @@
-AWS_PROFILE=AdministratorAccess-923675928909 terraform init -reconfigure
+#!/usr/bin/env bash
+set -euo pipefail
+
+export AWS_PROFILE=AdministratorAccess-923675928909 
+
+terraform init -reconfigure
 terraform workspace select dev || terraform workspace new dev
 terraform plan
 terraform apply
