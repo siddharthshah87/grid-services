12303b35db0d49a2179cba87075590594f663696
Siddharth Shah <siddharthshah87@gmail.com>
2025-07-28 14:08:44 -0700

    Update build_and_push.sh

--- openleadr/build_and_push.sh
+++ openleadr/build_and_push.sh
@@ -15,6 +15,8 @@ fi
 REPO_URI="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/openleadr-vtn"
 eval "$LOGIN_CMD" | docker login --username AWS --password-stdin "$REPO_URI"
 
+cd "$(dirname "$0")"
+
 echo "Building and pushing to $REPO_URI..."
 docker build -t openleadr-vtn .
 docker tag openleadr-vtn:latest "$REPO_URI:latest"
