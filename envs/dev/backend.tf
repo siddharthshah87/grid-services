1c78323533eeaa9d6fe168f05a05c11bb73d212d
Siddharth Shah <siddharthshah87@gmail.com>
2025-07-28 14:18:37 -0700

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket       = "tf-state-grid-services-923675928909-v2"
     key          = "dev/terraform.tfstate"
     region       = "us-west-2"
-    use_lockfile = true
     encrypt      = true
   }
 }
