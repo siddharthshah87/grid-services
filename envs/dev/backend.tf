4c84875af4f129df965a1bd16cb3871ded9609d5
Siddharth Shah <siddharthshah87@gmail.com>
2025-07-29 11:15:40 -0700

    refactor security group rules

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "tf-state-grid-services-923675928909-v2"
-    key          = "dev/terraform.tfstate"
-    region       = "us-west-2"
-    encrypt      = true
+    bucket  = "tf-state-grid-services-923675928909-v2"
+    key     = "dev/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
   }
 }
 
