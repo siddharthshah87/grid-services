7ab5e7680ab25f0218cc49a3cdea9284e0e61174
Siddharth Shah <siddharthshah87@gmail.com>
2025-07-29 17:04:05 -0700

    Update variables.tf

--- modules/iot-core/variables.tf
+++ modules/iot-core/variables.tf
@@ -1,23 +1,22 @@
 variable "prefix" {
-  description = "Prefix for all MQTT/Iot resources"
+  description = "Prefix for all AWS IoT resources (e.g., 'volttron', 'openadr')"
   type        = string
 }
 
 variable "enable_logging" {
-  description = "Enable MQTT logging rule"
+  description = "Whether to enable MQTT logging to S3"
   type        = bool
   default     = false
 }
 
 variable "iot_role_arn" {
-  description = "IAM role ARN for IoT rule to use"
+  description = "IAM role ARN used by IoT rule to write to S3"
   type        = string
   default     = ""
 }
 
 variable "s3_bucket" {
-  description = "S3 bucket name for MQTT logging"
+  description = "Name of the S3 bucket to store MQTT logs"
   type        = string
   default     = ""
 }
-
