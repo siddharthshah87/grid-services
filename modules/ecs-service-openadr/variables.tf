47f401fa56c5b6b9e05ea92db354a1f587248961
Siddharth Shah <siddharthshah87@gmail.com>
2025-07-30 12:00:20 -0700

    Inject DB env vars and gate migrations

--- modules/ecs-service-openadr/variables.tf
+++ modules/ecs-service-openadr/variables.tf
@@ -3,3 +3,29 @@ variable "environment_secrets" {
   default = []
 }
 
+variable "db_host" {
+  type    = string
+  default = ""
+}
+
+variable "db_user" {
+  type    = string
+  default = ""
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "db_name" {
+  type    = string
+  default = ""
+}
+
+variable "run_migrations_on_startup" {
+  type    = bool
+  default = true
+}
+
