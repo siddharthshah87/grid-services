176d02d1f21d64fcd21bfce3693cf28ebd43dd37
Siddharth Shah <siddharthshah87@gmail.com>
2025-08-05 10:12:04 -0700

    Expose VPC endpoint SG and add ingress rules

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,5 +3,8 @@ output "vpc_id" { value = aws_vpc.this.id }
 output "public_subnets" { value = aws_subnet.public[*].id }
 output "private_subnet_ids" { value = aws_subnet.private[*].id }
 output "cidr_block" { value = var.cidr_block }
-output "vpc_endpoints_security_group_id" { value = aws_security_group.vpc_endpoints.id }
+output "vpc_endpoints_security_group_id" {
+  description = "SG that protects all interface endpoints"
+  value       = aws_security_group.vpc_endpoints.id
+}
 
