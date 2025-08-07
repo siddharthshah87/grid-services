58058ddaba0544b6cdd31d16fc4128e2011d13a4
Siddharth Shah <siddharthshah87@gmail.com>
2025-08-06 17:57:45 -0700

    fmt and changesg

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,4 +7,8 @@ output "vpc_endpoints_security_group_id" {
   description = "SG that protects all interface endpoints"
   value       = aws_security_group.vpc_endpoints.id
 }
-
+output "iot_data_endpoint_dns" {
+  description = "Private DNS name for the com.amazonaws.<region>.iot.data interface endpoint"
+  # use the first (and only) DNS entry for the endpoint
+  value = aws_vpc_endpoint.interface["iot.data"].dns_entry[0].dns_name
+}
